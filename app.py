28329ac1bc4c0dc0fb15360d676915ed457333f5
Matthew Springer <Mspringer97@gmail.com>
2024-05-22 10:26:49 -0500

    Second push

--- app.py
+++ app.py
@@ -1,30 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 import psycopg2
 #RDS connection parameters
-ENDPOINT= ""
-PORT= ""
-USER= ""
-PASSWORD = ""
-REGION= ""
-DBNAME= ""
+ENDPOINT = "database-1.cvi40q4igbd6.us-east-2.rds.amazonaws.com"
+PORT = "5432"
+USER = "postgres"
+PASSWORD = "Student1"
+REGION = "us-east-2"
+DBNAME = "postgres"
 
 app=Flask(__name__)
 @app.route("/")
 def index():
-    return "index"
+    return render_template("index.html")
 @app.route('/students')
 def get_students():
 #gets the credentials from .aws/credentials
-try:
-    conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME,
-    user=USER, password=PASSWORD)
-    cur = conn.cursor()
-    cur.execute("""SELECT * from test""")
-    query_results = cur.fetchall()
-    print(query_results)
-    return(query_results)
-except Exception as e:
-    print("Database connection failed due to {}".format(e))
-    return("Database connection failed due to {}".format(e))
-if __name__ == "__main__":
-    app.run(debug=True)
+    try:
+        conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME,
+        user=USER, password=PASSWORD)
+        cur = conn.cursor()
+        cur.execute("""SELECT * from student""")
+        query_results = cur.fetchall()
+        print(query_results)
+        return(query_results)
+    except Exception as e:
+        print("Database connection failed due to {}".format(e))
+        return("Database connection failed due to {}".format(e))
+    if __name__ == "__main__":
+        app.run(debug=True)
